beb88c74a43eba007f5b4fe9114a50edce81450f
namtroo <cvnamt4@gmail.com>
2023-03-29 13:50:15 +0700

    1stver

--- notifier.py
+++ notifier.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+import telegram
+import subprocess
+import asyncio
+
 class Notifier:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, token, chat_id):
+        self.bot = telegram.Bot(token=token)
+        self.chat_id = chat_id
+    
+    async def send_notification(self):
+        # Send a message by taking output of config.py
+        message = subprocess.check_output(['python', 'config.py'])
+        message_str = message.decode()
+        await self.bot.send_message(chat_id=self.chat_id, text=message_str)
+    
+if __name__ == "__main__":
+    # Set up the bot and chat_id
+    # My group use to test
+    bot_token = '5778730071:AAHU3Ti-FCSnY4KqcIsqYrIUrgitMOb0kVw'
+    chat_id = '-1001714864670'
+    telegram_bot = Notifier(token=bot_token, chat_id=chat_id)
+    asyncio.run(telegram_bot.send_notification())
--- writer.py
+++ writer.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
+import sqlite3
+from datetime import datetime
 
 class Writer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, db_name): #db_name will define in config.py
+        self.conn = sqlite3.connect(db_name)
+        self.cur = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS system_info
+                            (id INTEGER PRIMARY KEY,
+                            date DATETIME NOT NULL,
+                            cpu_info REAL NOT NULL,
+                            gpu_info REAL NOT NULL,
+                            disk_info REAL NOT NULL,
+                            mem_info REAL NOT NULL,
+                            net_info REAL NOT NULL
+                            )''')
+        self.conn.commit()
+
+    def insert_data(self, cpu_info, gpu_info, disk_info, mem_info, net_info):
+        date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.cur.execute("INSERT INTO system_info (cpu_info, gpu_info, disk_info, mem_info, net_info, date) VALUE (?, ?, ?, ?, ?, ?)",
+                    (cpu_info, gpu_info, disk_info, mem_info, net_info, date_time))
+        self.conn.commit()
+
+    def get_data(self):
+        self.cur.execute("SELECT * FROM system_info")
+        rows = self.cur.fetchall()
+        return rows
+
+    def close_connection(self):
+        self.cur.close()
+        self.conn.close()
