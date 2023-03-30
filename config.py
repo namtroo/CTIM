09a7ea31f448c3bb0b80be236ac7717c2051700e
doclai <toi@doclai.com>
2023-03-30 09:49:25 +0700

    update config template with usage

--- config.py
+++ config.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 
-class Config:
-    def __init__(self):
-        pass
+import configparser
 
-    def get_config(self):
-        # get config from file
-        pass
+class Init():
 
-    
+    def __init__(self) -> None:
+        cf = configparser.ConfigParser()
+        cf.read("config.ini")
+
+        if "telegram" in cf.sections():
+            self.has_telegram = True
+            self.telegram_token = cf["telegram"]["token"]
+            self.telegram_chat_id = cf["telegram"]["chat_id"]
+        else:
+            self.has_telegram = False
+        
+        if "mode" in cf.sections():
+            self.run_mode = cf["mode"]["run"]
+        else:
+            self.run_mode = "web"
+        
+        if "sqlite" in cf.sections():
+            self.sqlite_path = cf["sqlite"]["path"]
+        else:
+            self.sqlite_path = "history.db"
+
+        if "exporter" in cf.sections():
+            self.has_exporter = True
+            self.export_format = cf["exporter"]["format"]
+        else:
+            self.has_exporter = False
+
